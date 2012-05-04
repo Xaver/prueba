f2913977e7ddb1cc1737c373f2bafea11728b548
Gustavo Molinari <info@xaver.com.ar>
2012-05-04 17:43:16 -0300

    primer commit

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/postgresql"
+load "config/recipes/nodejs"
+load "config/recipes/rbenv"
+load "config/recipes/check"
+
+server "192.168.1.152", :web, :app, :db, primary: true
+
+set :application, "prueba"
+set :user, "servidor"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:Xaver/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prueba::Application.config.secret_token = '9d74ecb4b8b668b647e27f2993f08e49b3d6e865bdade0b875a0ce9c29db95fa0f0ffecf0507b764c1d001d41e62b82219ab3fa4268987490c53c864c0bb9ebd'
